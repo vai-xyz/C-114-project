5a70bd7694a2d70927cba6a502f5c5cd3e81234a
vai-xyz <119789706+vai-xyz@users.noreply.github.com>
2023-03-26 16:59:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function preload(){
+}
+
+function setup(){
+    canvas=createCanvas(400, 400);
+    canvas.center();
+}
+
+function draw(){
+}
+
+function take_snapshot(){
+    save('my_filter_lipstick_image.jpg');
+}
